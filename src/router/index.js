1a73de9ece13406b323d43c64bb41bdd3614c948
Sándor Nagy <thesatyka@gmail.com>
2024-12-03 19:29:25 +0100

    router a guide pagehez létrehozva

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import List from "../components/List.vue";
 import Welcome from "../components/Welcome.vue";
 import { useUserStore } from "../../stores/user";
+import Guide from "../components/Guide.vue";
 
 const routes = [
     { path: "/", component: Welcome },
     { path: "/list", component: List },
+    { path: "/guide", component: Guide}
 ];
 
 const router = createRouter({
